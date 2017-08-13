9e38cee94f38f13d9be8ae122382805766314c8c
limeng <lm2343635@126.com>
2017-08-13 17:09:12 +0900

    Use recursive function to format sub JSON.

--- Example/MGFormatter/ViewController.swift
+++ Example/MGFormatter/ViewController.swift
@@ -10,9 +10,11 @@ import UIKit
 import SwiftyJSON
 import M80AttributedLabel
 
-let string = "{\"result\":{\"messages\":[{\"mid\":\"000000005cb02e93015cb12a3676007b\",\"createAt\":1497620952694,\"updateAt\":1497620952694,\"seq\":22,\"title\":\"Desk\",\"price\":1000,\"cover\":\"/files/picture/000000005cb02e93015cb12a3676007b/357cf653-e9b5-442c-a038-d933c47eaebf.jpg\",\"favorites\":2,\"enable\":true,\"cid\":\"000000005bce908d015bd18e59460006\"},{\"mid\":\"000000005cb02e93015cb0d5a51b0073\",\"createAt\":1497615410458,\"updateAt\":1497615410458,\"seq\":21,\"title\":\"Panasonic Hair dryer\",\"price\":1000,\"cover\":\"/files/picture/000000005cb02e93015cb0d5a51b0073/bc5921fa-ee0b-474b-bf54-f10206249f4c.jpg\",\"favorites\":4,\"enable\":true,\"cid\":\"000000005bce908d015bd18e59460006\"},{\"mid\":\"000000005cb02e93015cb0cdcc540051\",\"createAt\":1497614896212,\"updateAt\":1497614896212,\"seq\":15,\"title\":\"yuasa扇風機\",\"price\":1500,\"cover\":\"/files/picture/000000005cb02e93015cb0cdcc540051/f010c0c7-9431-4ba3-baa1-81440c2033e9.jpg\",\"favorites\":2,\"enable\":true,\"cid\":\"000000005bce908d015bd18e59460006\"},{\"mid\":\"000000005cb02e93015cb0c3df4f003b\",\"createAt\":1497614245709,\"updateAt\":1497614245709,\"seq\":12,\"title\":\"電子レンジ\",\"price\":5000,\"cover\":\"/files/picture/000000005cb02e93015cb0c3df4f003b/73c5fc14-6373-4450-ae40-b7da62e54c2d.jpg\",\"favorites\":1,\"enable\":true,\"cid\":\"000000005bce908d015bd18e59460006\"},{\"mid\":\"000000005cb02e93015cb0bfb43c0029\",\"createAt\":1497613972540,\"updateAt\":1497613972540,\"seq\":7,\"title\":\"Window air conditioner\",\"price\":8000,\"cover\":\"/files/picture/000000005cb02e93015cb0bfb43c0029/510d1a22-1f80-4c46-9198-7bfae6b892b6.jpg\",\"favorites\":1,\"enable\":true,\"cid\":\"000000005bce908d015bd18e59460006\"},{\"mid\":\"000000005cb02e93015cb0be48380022\",\"createAt\":1497613879352,\"updateAt\":1497613879352,\"seq\":6,\"title\":\"Sanyo refrigerator\",\"price\":4000,\"cover\":\"/files/picture/000000005cb02e93015cb0be48380022/7edc55ae-551e-49a8-ba40-5f5e99e6a632.jpg\",\"favorites\":1,\"enable\":true,\"cid\":\"000000005bce908d015bd18e59460006\"},{\"mid\":\"000000005cb02e93015cb09afa750005\",\"createAt\":1497611565668,\"updateAt\":1497611565668,\"seq\":1,\"title\":\"A good lamp \",\"price\":1000,\"cover\":\"/files/picture/000000005cb02e93015cb09afa750005/51bcb4a5-5a9a-4e87-95f0-9a7b14de55a5.jpg\",\"favorites\":3,\"enable\":true,\"cid\":\"000000005bce908d015bd18e59460006\"}]},\"status\":200,\"type\":\"application/json\",\"time\":2.56}"
+let string = "{\"result\":{\"messages\":[{\"mid\":\"000000005cb02e93015cb0be48380022\",\"createAt\":1497613879352,\"updateAt\":1497613879352,\"seq\":6,\"title\":\"Sanyo refrigerator\",\"price\":4000,\"cover\":\"/files/picture/000000005cb02e93015cb0be48380022/7edc55ae-551e-49a8-ba40-5f5e99e6a632.jpg\",\"favorites\":1,\"enable\":true,\"cid\":\"000000005bce908d015bd18e59460006\"},{\"mid\":\"000000005cb02e93015cb09afa750005\",\"createAt\":1497611565668,\"updateAt\":1497611565668,\"seq\":1,\"title\":\"A good lamp \",\"price\":1000,\"cover\":\"/files/picture/000000005cb02e93015cb09afa750005/51bcb4a5-5a9a-4e87-95f0-9a7b14de55a5.jpg\",\"favorites\":3,\"enable\":true,\"cid\":\"000000005bce908d015bd18e59460006\"}]},\"status\":200,\"type\":\"application/json\",\"time\":2.56,\"update\":true}"
 
 class ViewController: UIViewController {
+    
+    var tabs = 0
 
     let prettyLabel = M80AttributedLabel()
     
@@ -21,24 +23,7 @@ class ViewController: UIViewController {
         
         if let data = string.data(using: .utf8) {
             let json = JSON(data: data)
-            appendNormal("{\n")
-            for (key,subJson):(String, JSON) in json {
-                tab(1)
-                appendAttribute(key)
-                if let string = subJson.string {
-                    appendString(string)
-                } else if let double = subJson.double {
-                    if double.truncatingRemainder(dividingBy: 1) == 0 {
-                        appendNumber(String(format: "%.0f", double))
-                    } else {
-                        appendNumber(String(double))
-                    }
-                }
-                
-                newLine()
-            }
-            appendNormal("}")
-            
+            appendJSON(json)
         }
 
         let width = self.view.frame.size.width
@@ -59,6 +44,46 @@ class ViewController: UIViewController {
 
     }
     
+    func appendJSON(_ json: JSON) {
+        tabs += 1
+        appendNormal("{\n")
+        for (key, subJson):(String, JSON) in json {
+            appendTab(tabs)
+            appendAttribute(key)
+            if let boolean = subJson.bool {
+                appendNormal(boolean ? "true," : "false,")
+            } else if let string = subJson.string {
+                appendString(string)
+            } else if let double = subJson.double {
+                if double.truncatingRemainder(dividingBy: 1) == 0 {
+                    appendNumber(String(format: "%.0f", double))
+                } else {
+                    appendNumber(String(double))
+                }
+            } else if let array = subJson.array  {
+                appendNormal("[\n")
+                tabs += 1
+                appendTab(tabs)
+                for i in 0 ..< array.count {
+                    appendJSON(array[i])
+                    if (i < array.count - 1) {
+                        appendNormal(", ")
+                    }
+                }
+                tabs -= 1
+                newLine()
+                appendTab(tabs)
+                appendNormal("]")
+            } else {
+                appendJSON(subJson)
+            }
+            newLine()
+        }
+        tabs -= 1
+        appendTab(tabs)
+        appendNormal("}")
+    }
+    
     func appendNormal(_ text: String) {
         appendText(text, color: .black)
     }
@@ -68,15 +93,19 @@ class ViewController: UIViewController {
     }
     
     func appendNumber(_ text: String) {
-        appendText(text, color: .cyan)
+        appendText("\(text),", color: .cyan)
     }
     
     func appendString(_ text: String) {
-        appendText("\"\(text)\"", color: .blue)
+        appendText("\"\(text)\",", color: .blue)
     }
     
-    func tab(_ n: Int) {
-        appendNormal("  ");
+    func appendTab(_ n: Int) {
+        var tab = ""
+        for _ in 0 ..< n {
+            tab += "  "
+        }
+        appendNormal(tab);
     }
     
     func newLine() {
